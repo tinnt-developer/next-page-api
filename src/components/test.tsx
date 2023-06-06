c3d378f056c7b9e55fb5877530c6fdbeed8dba3e
Quy Phan <quyphan@Quys-MacBook-Pro-2.local>
2023-06-06 11:49:44 +0700

    init

---
+++ src/components/test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export type Props = {
+  className?: string;
+};
+
+const Test: React.FC<Props> = () => {
+  return <React.Fragment>hello</React.Fragment>;
+};
+
+export default Test;
